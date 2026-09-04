725a69b4ab1fab6edd081e23ebc9a717d181590f
devaas <aluinform@gmail.com>
2026-09-03 21:48:05 -0300

    Problema 02 (EC): Bhakara atualizado

--- EC/bhaskara.py
+++ EC/bhaskara.py
@@ -1,3 +1,5 @@
+import math
+
 a = float(input("Coeficiente a: "))
 b = float(input("Coeficiente b: "))
 c = float(input("Coeficiente c: "))
@@ -7,7 +9,7 @@ delta = b**2 - 4*a*c
 if delta < 0 or a == 0:
     print("Não há raízes reais.")
 else:
-    x1 = (-b + delta**0.5) / (2*a)
+    x1 = (-b + math.sqrt(delta)) / (2*a)
     x2 = (-b - delta**0.5) / (2*a)
 
     print(f"X1 = {x1:.4f}")
